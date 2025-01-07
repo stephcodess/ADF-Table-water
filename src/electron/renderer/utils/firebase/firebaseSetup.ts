909e47e8e4a8fe83673cca56ba4ba79be26cef6f
Stephen <rajiolaoluwa0@gmail.com>
2025-01-07 18:48:08 +0100

    fixes

--- src/electron/renderer/utils/firebase/firebaseSetup.ts
+++ src/electron/renderer/utils/firebase/firebaseSetup.ts
@@ -2,30 +2,17 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Firebase configuration with environment variables
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCSd1I0S9EcpjRfXQkIMRvzZvXLtJepRfY",
-//   authDomain: "adftable-water.firebaseapp.com",
-//   projectId: "adftable-water",
-//   storageBucket: "adftable-water.appspot.com",
-//   messagingSenderId: "33497998063",
-//   appId: "1:33497998063:web:13ca39c32c1c1e7c3f311a",
-//   measurementId: "G-98BS5LJ3C6",
-// };
-
-// the new database
 const firebaseConfig = {
-  apiKey: "AIzaSyBBKCSWUYMg9ldWrG9isz_YZl1ddWVwuaY",
-  authDomain: "adf-table-water.firebaseapp.com",
-  projectId: "adf-table-water",
-  storageBucket: "adf-table-water.appspot.com",
-  messagingSenderId: "616493639914",
-  appId: "1:616493639914:web:cf5fcc98e89ce1ae8d0f93",
-  measurementId: "G-YGYLNZQH0L",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 
-export const firestore = getFirestore(app); // Firestore service
-export const auth = getAuth(app); // Authentication service
-// export const analytics = getAnalytics(app); // Analytics (optional)
+export const firestore = getFirestore(app);
+export const auth = getAuth(app);
